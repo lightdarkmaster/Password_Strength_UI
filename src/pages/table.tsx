86b6d7900ceae10949fc5144f72b9e7f2a75f50d
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-04-09 20:44:30 +0800

    init Table

---
+++ src/pages/table.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function table() {
+  return (
+    <div className="sample">
+        <table>
+            <th>He</th>
+        </table>
+    </div>
+  );
+}
+
+export default table
